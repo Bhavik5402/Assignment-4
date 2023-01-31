87c4aaeec31f4b99dbaee8864b4bf48659376467
Bhavik5402 <105346981+Bhavik5402@users.noreply.github.com>
2023-01-31 18:14:39 +0530

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,35 @@
-burger= document.querySelector('.burger');
+tab_1=document.getElementById("tab-1");
+tab_2=document.getElementById("tab-2");
+tab_3=document.getElementById("tab-3");
+tab1_pane=document.getElementById("tab1-pane");
+tab2_pane=document.getElementById("tab2-pane");
+tab3_pane=document.getElementById("tab3-pane");
 
 
-burger.addEventListener('click' , ()=>{
-    navbar.classList.toggle('h-class');
-    navlist.classList.toggle('v-class');
-    rightsec.classList.toggle('v-class');
+tab_1.addEventListener("click", function(){
+    this.classList.add('active-link');
+    tab_2.classList.remove('active-link');
+    tab_3.classList.remove('active-link');
+    tab1_pane.classList.add('active');
+    tab2_pane.classList.remove('active');
+    tab3_pane.classList.remove('active');
+    
+});
+tab_2.addEventListener("click", function(){
+    this.classList.add('active-link');
+    tab_1.classList.remove('active-link');
+    tab_3.classList.remove('active-link');
+    tab1_pane.classList.remove('active');
+    tab2_pane.classList.add('active');
+    tab3_pane.classList.remove('active');
+});
+tab_3.addEventListener("click", function(){
+    this.classList.add('active-link');
+    tab_1.classList.remove('active-link');
+    tab_2.classList.remove('active-link');
+    tab1_pane.classList.remove('active');
+    tab2_pane.classList.remove('active');
+    tab3_pane.classList.add('active');
+});
 
-})
+
